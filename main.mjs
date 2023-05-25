9957e3b1e50d0337d4cab660bb2eab59dfa3224e
JoshuaHerring <jdherring2222@gmail.com>
2023-05-25 13:27:19 -0600

    added hitbox for mewoknight

--- main.mjs
+++ main.mjs
@@ -1,6 +1,7 @@
 import pop from "./pop/index.js";
 const { Game, KeyControls } = pop;
 import Player from "./entities/player/player.mjs";
+import entity from "./pop/utils/entity.js";
 
 const game = new Game(window.innerWidth, window.innerHeight);
 const {scene, w, h} = game; 
@@ -11,6 +12,8 @@ for(let i = 0; i < 30; i++) {
     const player = new Player(controls);
     player.pos = {x: window.innerWidth / 2, y: window.innerHeight / 2};
     scene.add(player); 
+    entity.addDebug(player)
 }
+
 
 game.run();

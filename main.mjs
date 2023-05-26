f12b39d0abf5ce9cea66c67c265a550c360074b8
JoshuaHerring <jdherring2222@gmail.com>
2023-05-25 19:09:23 -0600

    added functioning camera and a temp map for tests

--- main.mjs
+++ main.mjs
@@ -1,19 +1,27 @@
 import pop from "./pop/index.js";
-const { Game, KeyControls } = pop;
+const { Game, KeyControls, Camera } = pop;
 import Player from "./entities/player/player.mjs";
 import entity from "./pop/utils/entity.js";
+import Level from "./resources/Levels/testLevel.mjs";
 
-const game = new Game(window.innerWidth, window.innerHeight);
+const game = new Game(window.innerWidth - 8, window.innerHeight - 20);
 const {scene, w, h} = game; 
-
+const level = new Level(w * 4, h * 4)
 const controls = new KeyControls(); 
 
-// for(let i = 0; i < 30; i++) {
-    const player = new Player(controls);
-    player.pos = {x: window.innerWidth / 2, y: window.innerHeight / 2};
-    scene.add(player); 
-    // entity.addDebug(player)
-// }
+const player = new Player(controls);
+player.pos = {x: w / 2, y: h / 2};
 
+
+
+
+const camera = scene.add(new Camera(player, {w: w, h: h}, {w: level.w, h: level.h}))
+
+camera.add(level)
+camera.add(player); 
+
+
+
+    
 
 game.run();

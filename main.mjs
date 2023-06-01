9c09258f30f3ffb77dfeb91d5bf997a3aafabc1e
Jake Zalesny <jrzalesny@gmail.com>
2023-06-01 13:04:35 -0600

    added dodges and finished attack animations

--- main.mjs
+++ main.mjs
@@ -3,13 +3,15 @@ const { Game, KeyControls, Camera } = pop;
 import Player from "./entities/player/player.mjs";
 import entity from "./pop/utils/entity.js";
 import Level from "./resources/Levels/testLevel.mjs";
+import MouseControls from "./pop/controls/MouseControls.js";
 
 const game = new Game(window.innerWidth, window.innerHeight - 4);
 const {scene, w, h} = game; 
 const level = new Level(w * 4, h * 4)
 const controls = new KeyControls(); 
+const mousecontrols = new MouseControls(); 
 
-const player = new Player(controls);
+const player = new Player(controls, mousecontrols);
 player.pos = {x: w / 2, y: h / 2};
 
 
